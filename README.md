c0b27ff3185ded1b4595809b22533fc40c26078c
Alek Westover <alek.westover@gmail.com>
2019-03-26 12:54:25 -0400

    version 21

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # theland2
 theland2
+
+# draft1:
+  massive grid of things
+  little guys
+  when its movement time
+  you tell them all where to go
+  there is a 60 sec interval where they can do that
+  then the movement phase
+  then collisions can happen
+  as the sequential movement thing happens 
+
+
+#Simplest:
+  all squares black but 1 white one
+  click it during the move phase and move it
+  then do the move pahse
+
+# simplest version 2
--- sketch.js
+++ sketch.js
@@ -1,32 +1,65 @@
 
-let stupidColors = [];
-let sqs = 10; // number of squares
+let sqs = 10; // number of squares per side of the grid 
 let sqd = 30; // square side length
 // prob should be rectangle, because most people have rectangular screens
 
-function crumbColors() {
+let planningPhase = true; // PLAN or BUILD
+
+let colorMatrix = [];
+for(var i = 0; i < sqs; i++){
+	colorMatrix.push([]);
+	for(var j = 0; j < sqs; j++){
+		colorMatrix[i].push(null);
+	}
+}
+
+let userPos = [Math.floor(sqs/2), Math.floor(sqs/2)];
+
+function updateBoard(userPos) {
 	for(var i = 0; i < sqs; i++){
-		stupidColors.push([]);
-		for (var j = 0; j < sqs; j++){
-			stupidColors[i].push(color(random()*255,random()*255,random()*255));
+			for (var j = 0; j < sqs; j++){
+				colorMatrix[i][j] = color(0,0,0);
 		}
 	}
+	colorMatrix[userPos[0]][userPos[1]] = color(255,255,255);
 }
 
 function setup() {
 	createCanvas(sqs*sqd,sqs*sqd);
-	crumbColors();
+	background(0,0,0);
+	updateBoard(userPos);
+	showBoard();
+}
+
+function showBoard() {
+	for(var i = 0; i < sqs; i++) {
+		for(var j = 0; j < sqs; j++) {
+			fill(colorMatrix[i][j]);
+			rect(sqd*i,sqd*j,sqd,sqd);
+		}
+	}
+}
+
+
+function mouseReleased() {
+	if(planningPhase) {
+		if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+
+			let xIdx = floor(mouseX / sqd);
+			let yIdx = floor(mouseY / sqd);
+			userPos = [xIdx, yIdx];
+			updateBoard(userPos);
+			showBoard();
+		}
+	}
 }
 
 
 function draw() {
-	background(0,0,0);
-
-	for(var i = 0; i < sqs; i++) {
-		for(var j = 0; j < sqs; j++) {
-			fill(stupidColors[i][j]);
-			rect(sqd*i,sqd*j,sqd,sqd);
-		}
+	if(planningPhase) {
+	}
+	else {
+		showBoard();
 	}
 }
 
